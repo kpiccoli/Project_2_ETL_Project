ef7383f152bf52f7ff4753f35af5f3ace26f8ba6
Karine Piccoli <61287395+kpiccoli@users.noreply.github.com>
2020-06-01 18:34:06 -0400

    sql

---
+++ ETL_Project.sql
@@ -0,0 +1,27 @@
+CREATE TABLE final_marvel (
+	page_id INT,
+	name VARCHAR,
+	id VARCHAR,
+	align VARCHAR,
+	alive VARCHAR,
+	appearances FLOAT,
+	first_appearance VARCHAR,
+	year FLOAT
+);
+
+SELECT *
+FROM final_marvel;
+
+CREATE TABLE final_dc (
+page_id INT,
+	name VARCHAR,
+	id VARCHAR,
+	align VARCHAR,
+	alive VARCHAR,
+	appearances FLOAT,
+	first_appearance VARCHAR,
+	year FLOAT
+);
+
+SELECT *
+FROM final_dc;
